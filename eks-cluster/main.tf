54f49eecace6da91719de5e1ea6e0f2dc97a1235
Carlyle S. Davis <carlyle.davis@gmail.com>
2026-08-08 12:35:50 -0400

    Allowing for 2 nodes.

--- eks-cluster/main.tf
+++ eks-cluster/main.tf
@@ -79,9 +79,9 @@ module "eks" {
   eks_managed_node_groups = {
 
     core = {
-      min_size     = 1
-      max_size     = 1
-      desired_size = 1
+      min_size     = 2
+      max_size     = 2
+      desired_size = 2
 
       # instance_types = [var.instance_type]
       instance_types = ["m8a.xlarge"]
